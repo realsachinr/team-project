8ff344e3228fccb34de85be8064d0cd6ca50f948
Mukesh Kumar <mukesh512004@gmail.com>
2025-04-08 17:09:25 +0530

    ContactUs.jsx file added! in components folder and remove defaults code of App.jsx

---
+++ src/component/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <>
+        <div className='container text-center text-danger'>
+            <p>This is Contact us Section created by Mukesh Kumar</p>
+        </div>
+    </>
+  )
+}
+
+export default ContactUs
